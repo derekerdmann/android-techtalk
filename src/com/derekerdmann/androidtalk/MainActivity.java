f10114000a268c3b2a29e97676fcf7ab18f6d302
Derek Erdmann <derek@derekerdmann.com>
2011-09-29 14:53:55 -0400

    Added simple GUI to MainActivity

--- src/com/derekerdmann/androidtalk/MainActivity.java
+++ src/com/derekerdmann/androidtalk/MainActivity.java
@@ -2,12 +2,27 @@ package com.derekerdmann.androidtalk;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
+/**
+ * Main activity for the Stack Overflow browser.  Displays some basic 
+ * information about Stack Overflow
+ * 
+ * @author Derek Erdmann
+ */
 public class MainActivity extends Activity {
-    /** Called when the activity is first created. */
+    
+	
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+    
+    
+    public void onQuestionsButtonClick( View v ){
+    	
+    }
+    
 }
