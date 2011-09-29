3364af1e3dd8d25f95e10792b726fa5563ac7e97
Derek Erdmann <derek@derekerdmann.com>
2011-09-29 15:14:14 -0400

    Updated MainActivity with AsyncTask to get the total number of questions (dummy for now)

--- src/com/derekerdmann/androidtalk/MainActivity.java
+++ src/com/derekerdmann/androidtalk/MainActivity.java
@@ -1,9 +1,12 @@
 package com.derekerdmann.androidtalk;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 /**
  * Main activity for the Stack Overflow browser.  Displays some basic 
@@ -18,7 +21,17 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+    	setContentView(R.layout.main);
+    }
+    
+    
+    /**
+     * Called when the activity starts
+     */
+    @Override
+    public void onStart(){
+    	super.onStart();
+    	new QuestionTotalTask().execute();
     }
     
     
@@ -29,5 +42,45 @@ public class MainActivity extends Activity {
     public void onQuestionsButtonClick( View v ){
     	startActivity( new Intent( this, QuestionsActivity.class ) );
     }
+    
+    
+    
+    /**
+     * Gets the total number of questions on Stack Overflow
+     * @author Derek Erdmann
+     */
+    class QuestionTotalTask extends AsyncTask<Void, Void, Integer>{
+
+    	private ProgressDialog progress;
+    	
+    	@Override
+    	protected void onPreExecute(){
+    		progress = new ProgressDialog( MainActivity.this );
+    		progress.setMessage( "Counting..." );
+    		progress.show();
+    	}
+    	
+		@Override
+		protected Integer doInBackground(Void... arg0) {
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			return 5;
+		}
+		
+		
+		@Override
+		protected void onPostExecute( Integer result ){
+			
+			TextView total = (TextView) findViewById( R.id.total );
+			total.setText( result.toString() );
+			
+			progress.dismiss();
+		}
+    	
+    }
+    
     
 }
