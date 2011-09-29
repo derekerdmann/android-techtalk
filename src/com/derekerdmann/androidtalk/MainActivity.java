33eb2414462c7ea06be1ed52d9834715d88c2dc1
Derek Erdmann <derek@derekerdmann.com>
2011-09-29 16:56:25 -0400

    Added request to read StackOverflow question total

--- src/com/derekerdmann/androidtalk/MainActivity.java
+++ src/com/derekerdmann/androidtalk/MainActivity.java
@@ -1,10 +1,26 @@
 package com.derekerdmann.androidtalk;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.zip.GZIPInputStream;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -15,6 +31,10 @@ import android.widget.TextView;
  * @author Derek Erdmann
  */
 public class MainActivity extends Activity {
+	
+	
+	private static final String question_total_url = 
+			"http://api.stackoverflow.com/1.1/questions?pagesize=0";
     
 	
 	/** Called when the activity is first created. */
@@ -50,7 +70,9 @@ public class MainActivity extends Activity {
      * @author Derek Erdmann
      */
     class QuestionTotalTask extends AsyncTask<Void, Void, Integer>{
-
+    	
+    	private static final String tag = "QuestionTotalTask";
+    	
     	private ProgressDialog progress;
     	
     	@Override
@@ -62,12 +84,33 @@ public class MainActivity extends Activity {
     	
 		@Override
 		protected Integer doInBackground(Void... arg0) {
+			
+			int result = 0;
+			
 			try {
-				Thread.sleep(3000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+				HttpClient client = new DefaultHttpClient();
+				HttpGet get = new HttpGet( question_total_url );
+				
+				HttpResponse response = client.execute( get );
+				HttpEntity entity = response.getEntity();
+				
+				if( entity != null ){
+					
+					String json = unzipResponse( entity.getContent() );
+					
+					Log.v( tag, json );
+					result = parseTotal( json );
+				}
+
+			} catch (ClientProtocolException e) {
+				Log.e(tag, e.getMessage() );
+				Log.e( tag, Log.getStackTraceString( e ) );
+			} catch (IOException e) {
+				Log.e(tag, e.getMessage() );
+				Log.e( tag, Log.getStackTraceString( e ) );
 			}
-			return 5;
+			
+			return result;
 		}
 		
 		
@@ -79,8 +122,57 @@ public class MainActivity extends Activity {
 			
 			progress.dismiss();
 		}
+		
+		
+		/**
+		 * Unzips the response input stream
+		 * @param in - the input stream to unzip
+		 * @return Returns the content of the response1
+		 */
+		protected String unzipResponse( InputStream in ){
+			
+			StringBuilder builder = new StringBuilder();
+			String line = null;
+			
+			try {
+				
+				GZIPInputStream gzip = new GZIPInputStream( in );
+				
+				BufferedReader reader = 
+						new BufferedReader( new InputStreamReader( gzip ) );
+					
+				while( ( line = reader.readLine() ) != null ){
+					builder.append( line );
+				}
+				
+				reader.close();
+				
+			}catch( IOException e ){
+				Log.e(tag,  e.getMessage() );
+				Log.e( tag, Log.getStackTraceString( e ) );
+			}
+			
+			return builder.toString();
+			
+		}
+		
+		/**
+		 * Parses the question total from the JSON string
+		 * @param json - Returns the total number of questions in the request
+		 */
+		protected int parseTotal( String json ){
+			try {
+				JSONObject obj = new JSONObject( json );
+				String totalString = obj.getString( "total" );
+				
+				return Integer.parseInt( totalString );
+				
+			} catch (JSONException e) {
+				Log.e(tag, e.getMessage() );
+				return 0;
+			}
+		}
     	
     }
-    
     
 }
